7c72d9f27b616ecc0d829788e45dec5d3f0ce2a9
tuckerward20 <tuckerward20@gmail.com>
2021-01-25 14:42:59 -0600

    first upload

---
+++ SwitchStatementsExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwitchStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Rich Nerd");
+                    break;
+                case "Science":
+                    Console.WriteLine("Nerd");
+                    break;
+                case "English":
+                    Console.WriteLine("Poor Nerd");
+                    break;
+                case "History":
+                    Console.WriteLine("Really Poor Nerd");
+                    break;
+                case "Philosophy":
+                    Console.WriteLine("Poor Nerd?");
+                    break;
+                default:
+                    Console.WriteLine("Try Math, Science, English, History or Philosophy");
+                    break;
+            }
+                
+        }
+    }
+}
